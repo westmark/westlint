6e39d916f12c7552fed9d072700f3eaf92fc50b3
Fredrik Westmark <fredrik.westmark@tre.se>
2018-11-23 09:04:04 +0100

    Use prettier rules for style, only

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-const configure = require( './config' );
+module.exports = {
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier', 'jest'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        parser: 'babylon',
+        bracketSpacing: true,
+        printWidth: 100,
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+      },
+    ],
+    // React
+    'react/jsx-filename-extension': 0,
+    'react/destructuring-assignment': 0,
 
-const eslintConfig = configure( [ './node_modules' ] );
+    'import/prefer-default-export': 0,
 
-module.exports = eslintConfig;
+    // Jest
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
+  settings: {},
+  parser: 'babel-eslint',
+  env: {
+    'jest/globals': true,
+  },
+};
